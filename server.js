ec0043bbda34e374aa6aebedaa3448cf8d000499
Rivetta <claudio.rivetta@capgemini.com>
2026-04-03 09:51:35 +0200

    Change port to 3001 and finalize journey status management

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
